5d6392fd1954ce899266226ba4b2a2918af33019
Akashamba <53367477+Akashamba@users.noreply.github.com>
2020-02-25 06:34:49 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,5 +1,28 @@
+import PySimpleGUI as s
 from Log_In import login
-from Search import search
+from Sign_Up import signup
 
-if login():
-    search()
+col = "#343434"
+
+layout = [
+    [s.Text("Student Contact Manager", font='courier' '50', background_color=col, justification="left")],
+    [s.Text("", background_color=col)],
+    [s.Button("Log In", button_color=('white', 'black')), s.Button("Sign Up", button_color=('white', 'black'))]
+]
+w = s.Window("Student Contact Manager", layout, background_color=col, use_default_focus=False, element_justification="center")
+while True:
+    button, values = w.Read()
+
+    if button == "Log In":
+        w.Close()
+        login()
+        break
+
+    if button == "Sign Up":
+        w.Close()
+        signup()
+        break
+
+    if button is None:
+        w.Close()
+        break
