d35fa2b9870e0f96a9b717a7301b21ebd9bae50b
Akashamba <53367477+Akashamba@users.noreply.github.com>
2020-02-25 06:34:13 +0530

    Update Sign_Up.py

--- Sign_Up.py
+++ Sign_Up.py
@@ -1,16 +1,18 @@
 import PySimpleGUI as s
 from back import *
+import Log_In
 
 col = "#343434"
 
 def signup():
     sign_up_layout = [
+        [s.Text("Student Contact Manager", font='courier' '50', background_color=col, justification="left")],
         [s.Text("Enter Name         ", background_color=col), s.InputText("", key='name', size=(20,10))],
         [s.Text("Enter Username   ", background_color=col), s.InputText("", key='username', size=(20,10))],
         [s.Text("Enter Password   ", background_color=col), s.InputText("", key="password", password_char='*', size=(20,10))],
         [s.Text("Confirm Password", background_color=col), s.InputText("", key="cpassword", password_char='*',size=(20,10))],
         [s.Text("                                   ", key='msg', background_color=col)],
-        [s.Button("Create Account", bind_return_key=True,   button_color=('white', 'black')), s.Button("Log In",   button_color=('white', 'black'))]
+        [s.Button("Create Account", bind_return_key=True,   button_color=('white', 'black')), s.Button("Log In", button_color=('white', 'black'))]
     ]
 
     sign_w = s.Window("Sign Up", sign_up_layout, resizable=1, background_color=col)
@@ -31,7 +33,12 @@ def signup():
             else:
                 sign_w.FindElement('msg').update("Passwords do not match",  text_color='red')
 
-        if s_button == None:
+        if s_button == "Log In":
+            sign_w.Close()
+            Log_In.login()
+            break
+
+        if s_button is None:
             sign_w.Close()
             break
 
